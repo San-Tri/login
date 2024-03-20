d70b79ed4a4a61b063a6238228bb5bbcc383b687
ALUMNO <ALUMNO@E10KLAB401PC22>
2024-03-20 11:23:42 -0500

    Agregar archivos de proyecto.

---
+++ login/Form1.Designer.cs
@@ -0,0 +1,198 @@
+﻿namespace login
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            label1 = new Label();
+            label2 = new Label();
+            label3 = new Label();
+            textBox1 = new TextBox();
+            textBox2 = new TextBox();
+            button1 = new Button();
+            linkLabel1 = new LinkLabel();
+            linkLabel2 = new LinkLabel();
+            checkBox1 = new CheckBox();
+            label4 = new Label();
+            label5 = new Label();
+            SuspendLayout();
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.BackColor = SystemColors.WindowFrame;
+            label1.Font = new Font("Comic Sans MS", 14.25F, FontStyle.Bold | FontStyle.Italic, GraphicsUnit.Point, 0);
+            label1.ForeColor = SystemColors.ButtonHighlight;
+            label1.Location = new Point(167, 41);
+            label1.Name = "label1";
+            label1.Size = new Size(57, 27);
+            label1.TabIndex = 0;
+            label1.Text = "Login";
+            // 
+            // label2
+            // 
+            label2.AutoSize = true;
+            label2.Font = new Font("Comic Sans MS", 12F, FontStyle.Bold | FontStyle.Italic, GraphicsUnit.Point, 0);
+            label2.ForeColor = SystemColors.ButtonHighlight;
+            label2.Location = new Point(35, 111);
+            label2.Name = "label2";
+            label2.Size = new Size(108, 23);
+            label2.TabIndex = 1;
+            label2.Text = "Email addres";
+            // 
+            // label3
+            // 
+            label3.AutoSize = true;
+            label3.Font = new Font("Comic Sans MS", 12F, FontStyle.Bold | FontStyle.Italic, GraphicsUnit.Point, 0);
+            label3.ForeColor = SystemColors.ButtonHighlight;
+            label3.Location = new Point(35, 224);
+            label3.Name = "label3";
+            label3.Size = new Size(80, 23);
+            label3.TabIndex = 2;
+            label3.Text = "Password";
+            // 
+            // textBox1
+            // 
+            textBox1.Location = new Point(35, 142);
+            textBox1.Name = "textBox1";
+            textBox1.Size = new Size(318, 23);
+            textBox1.TabIndex = 3;
+            // 
+            // textBox2
+            // 
+            textBox2.Location = new Point(37, 253);
+            textBox2.Name = "textBox2";
+            textBox2.Size = new Size(316, 23);
+            textBox2.TabIndex = 4;
+            // 
+            // button1
+            // 
+            button1.BackColor = Color.DarkOrange;
+            button1.Font = new Font("Comic Sans MS", 9F, FontStyle.Bold | FontStyle.Italic, GraphicsUnit.Point, 0);
+            button1.Location = new Point(102, 347);
+            button1.Name = "button1";
+            button1.Size = new Size(204, 29);
+            button1.TabIndex = 5;
+            button1.Text = "Login";
+            button1.UseVisualStyleBackColor = false;
+            // 
+            // linkLabel1
+            // 
+            linkLabel1.AutoSize = true;
+            linkLabel1.Font = new Font("Segoe UI", 9F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            linkLabel1.LinkColor = Color.FromArgb(255, 128, 0);
+            linkLabel1.Location = new Point(293, 285);
+            linkLabel1.Name = "linkLabel1";
+            linkLabel1.Size = new Size(57, 15);
+            linkLabel1.TabIndex = 6;
+            linkLabel1.TabStop = true;
+            linkLabel1.Text = "Password";
+            // 
+            // linkLabel2
+            // 
+            linkLabel2.AutoSize = true;
+            linkLabel2.Font = new Font("Segoe UI", 9F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            linkLabel2.LinkColor = Color.FromArgb(255, 128, 0);
+            linkLabel2.Location = new Point(248, 404);
+            linkLabel2.Name = "linkLabel2";
+            linkLabel2.Size = new Size(53, 15);
+            linkLabel2.TabIndex = 7;
+            linkLabel2.TabStop = true;
+            linkLabel2.Text = "Registrar";
+            // 
+            // checkBox1
+            // 
+            checkBox1.AutoSize = true;
+            checkBox1.Font = new Font("Comic Sans MS", 9F, FontStyle.Bold | FontStyle.Italic, GraphicsUnit.Point, 0);
+            checkBox1.ForeColor = SystemColors.ButtonFace;
+            checkBox1.Location = new Point(37, 282);
+            checkBox1.Name = "checkBox1";
+            checkBox1.Size = new Size(143, 22);
+            checkBox1.TabIndex = 8;
+            checkBox1.Text = "Mostrar contraseña";
+            checkBox1.UseVisualStyleBackColor = true;
+            // 
+            // label4
+            // 
+            label4.AutoSize = true;
+            label4.Font = new Font("Segoe UI", 9F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            label4.ForeColor = SystemColors.ButtonHighlight;
+            label4.Location = new Point(113, 404);
+            label4.Name = "label4";
+            label4.Size = new Size(129, 15);
+            label4.TabIndex = 9;
+            label4.Text = "¿No tienes una cuenta?";
+            // 
+            // label5
+            // 
+            label5.AutoSize = true;
+            label5.ForeColor = SystemColors.ButtonHighlight;
+            label5.Location = new Point(220, 285);
+            label5.Name = "label5";
+            label5.Size = new Size(67, 15);
+            label5.TabIndex = 10;
+            label5.Text = "Restablecer";
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            BackColor = SystemColors.WindowFrame;
+            ClientSize = new Size(395, 495);
+            Controls.Add(label5);
+            Controls.Add(label4);
+            Controls.Add(checkBox1);
+            Controls.Add(linkLabel2);
+            Controls.Add(linkLabel1);
+            Controls.Add(button1);
+            Controls.Add(textBox2);
+            Controls.Add(textBox1);
+            Controls.Add(label3);
+            Controls.Add(label2);
+            Controls.Add(label1);
+            ForeColor = SystemColors.ButtonHighlight;
+            Name = "Form1";
+            Text = "Form1";
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private Label label1;
+        private Label label2;
+        private Label label3;
+        private TextBox textBox1;
+        private TextBox textBox2;
+        private Button button1;
+        private LinkLabel linkLabel1;
+        private LinkLabel linkLabel2;
+        private CheckBox checkBox1;
+        private Label label4;
+        private Label label5;
+    }
+}
